df21a37e32c6a9f3afdede8c95152e112329fc9d
binraw <truvelotrobin@yahoo.fr>
2024-02-26 10:19:10 +0100

    fix

--- src/main.c
+++ src/main.c
@@ -38,6 +38,7 @@ int pipex_process(char **argv, char ** envp, int *fd)
         second_child_process(argv, envp, fd);
 
     waitpid(first_child, NULL, 0);
+     return (0);
     
 }
 
@@ -47,12 +48,13 @@ int child_process(char **argv, char **envp, int *fd)
 	char	**command;
 	char	*path_command;
 	
-	command = create_cmd(argv, 2);  
+	command = create_cmd(argv, 3);  
 	path_command = create_path(command[0], envp);
 	filein = open(argv[1], O_RDONLY);
     dup2(filein, 0);
     dup2(fd[1], 1);
     execve(path_command, command, envp);
+     return (0);
 }
 
 char 	**create_cmd(char **argv, int i)
@@ -70,11 +72,12 @@ int second_child_process(char **argv, char **envp, int *fd)
 	char	*command;
 	char	*path_command;
 
-	command = create_cmd(argv, 3);
+	command = create_cmd(argv, 4);
 	path_command = create_path(command, envp);
     fileout = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC);
     dup2(fd[1], 0);
     dup2(fileout, 1);
     execve(path_command, command, envp);
+     return (0);
 }
 
