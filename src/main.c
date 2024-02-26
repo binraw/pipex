c5d56e05892a89c4e4d1cc44dbb824e952f9014a
binraw <truvelotrobin@yahoo.fr>
2024-02-26 10:41:54 +0100

    fix

--- src/main.c
+++ src/main.c
@@ -73,7 +73,7 @@ int second_child_process(char **argv, char **envp, int *fd)
 	char	*path_command;
 
 	command = create_cmd(argv, 4);
-	path_command = create_path(command, envp);
+	path_command = create_path(command[0], envp);
     fileout = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0666);
     dup2(fd[1], 0);
     dup2(fileout, 1);
